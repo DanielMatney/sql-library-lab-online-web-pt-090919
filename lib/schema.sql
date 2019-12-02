1f0ba562720f27d773d2d178dee61d113692dff5
Daniel Matney <danieljmatney@gmail.com>
2019-12-02 10:33:34 +0000

    Automatically backed up by Learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE Series (id INTEGER PRIMARY KEY, title TEXT, author TEXT, sub-genre TEXT);
-CREATE TABLE Sub-Genres (title TEXT);
-CREATE TABLE Authors (name TEXT);
-CREATE TABLE Books (title TEXT, year INTEGER, series TEXT);
-CREATE TABLE Characters (name TEXT, motto TEXT, species TEXT, author TEXT);
-CREATE TABLE character_books ()
+CREATE TABLE Sub-Genres (id INTEGER PRIMARY KEY, title TEXT);
+CREATE TABLE Authors (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE Books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series TEXT);
+CREATE TABLE Characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, author TEXT);
+CREATE TABLE character_books (id INTEGER PRIMARY KEY, character_id, book_id);
